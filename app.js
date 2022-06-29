e9fed65694a73e13c988c97f38caec4f5a41fe2c
Sungwoo Kim <paulkim9292@gmail.com>
2022-06-29 18:39:56 +0900

    add props, update README.md

--- app.js
+++ app.js
@@ -1,62 +1,23 @@
-function MinutesToHours() {
+function Converter(props) {
     const [amount, setAmount] = React.useState(0);
     const [invert, setInvert] = React.useState(false);
     return (
         <div>
-            <span>Minutes: </span>
+            <span>{props.unit1}</span>
             <input
                 id="min"
                 type="number"
-                value={invert ? amount*60 : amount}
+                value={invert ? amount*props.value1 : amount}
                 onChange={(event) => setAmount(event.target.value)}
                 disabled={invert}
             />
             <br/>
 
-            <span>Hours: </span>
+            <span>{props.unit2}</span>
             <input
                 id="hr"
                 type="number"
-                value={invert ? amount : amount/60}
-                onChange={(event) => setAmount(event.target.value)}
-                disabled={!invert}
-            />
-            <br/>
-            
-            <button
-                onClick={() => setAmount(0)}
-            >Reset</button>
-
-            <button
-                onClick={() => {
-                    setAmount(0);
-                    setInvert((current) => !current);
-                }}
-            >Invert</button>
-        </div>
-    );
-}
-
-function KmToMiles() {
-    const [amount, setAmount] = React.useState(0);
-    const [invert, setInvert] = React.useState(false);
-    return (
-        <div>
-            <span>KM: </span>
-            <input
-                id="km"
-                type="number"
-                value={invert ? amount*1.60934 : amount}
-                onChange={(event) => setAmount(event.target.value)}
-                disabled={invert}
-            />
-            <br/>
-
-            <span>Miles: </span>
-            <input
-                id="miles"
-                type="number"
-                value={invert ? amount : amount*0.621371}
+                value={invert ? amount : amount*props.value2}
                 onChange={(event) => setAmount(event.target.value)}
                 disabled={!invert}
             />
@@ -91,8 +52,18 @@ function App() {
                 <option value="1">KM & Miles</option>
             </select>
             <br/>
-            {index === "0" ? <MinutesToHours /> : null}
-            {index === "1" ? <KmToMiles /> : null}
+            {index === "0" ? <Converter 
+                unit1="Minutes: "
+                unit2="Hours: "
+                value1={60}
+                value2={1/60}
+            /> : null}
+            {index === "1" ? <Converter
+                unit1="KM: "
+                unit2="Miles: "
+                value1={1.60934}
+                value2={0.621371}
+            /> : null}
         </div>
     );
 }
